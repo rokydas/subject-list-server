e2db969aaa1635d0ddcbafee28f2023689f4215b
rokydas <droky136@gmail.com>
2020-12-04 22:32:23 +0600

    some api added

--- index.js
+++ index.js
@@ -1,15 +1,69 @@
-const express = require('express'); 
-const bodyParser = require('body-parser'); 
+const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload');
+require('dotenv').config()
 
-const app = express(); 
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('services'));
+app.use(fileUpload());
 
-app.get('/', (req, res) => {
-    res.send('Hello I am your new node js project');
-})
+const user = process.env.USER;
+const password = process.env.PASSWORD;
+const dbName = process.env.DB_NAME;
 
-app.listen(5000); 
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${user}:${password}@cluster0.muwip.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const subjectCollection = client.db(`${dbName}`).collection('subjects');
+
+    app.get('/', (req, res) => {
+        res.send('Hello I am your new node js project');
+    })
+
+    app.get('/subjects', (req, res) => {
+        subjectCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/insert', (req, res) => {
+        const file = req.files.file;
+        const classNo = req.body.classNo;
+        const subjectName = req.body.subjectName;
+        const subjectCode = req.body.subjectCode;
+        const topic = req.body.topic;
+        const type = req.body.type;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        const img = {
+            contextType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        }
+
+        subjectCollection.insertOne({ classNo, subjectName, subjectCode, topic, type, img })
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+
+    })
+
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 5000);  
